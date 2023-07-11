a01b9e3072f6544ee29ea67cd13d705dbd079a01
Tony Cavella <9434191+acavella@users.noreply.github.com>
2023-07-10 20:54:40 -0400

    new framework

--- est-reenroll.sh
+++ est-reenroll.sh
@@ -12,51 +12,44 @@ set -u
 ######## VARIABLES #########
 
 # Base directories
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__certs=${__dir}/certs)
-
-# Global Variables
-VERSION="0.0.1"
-DETECTED_OS=$(cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2- | tr -d '"')
+basedir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+tempdir=$(mktemp -d /tmp/est.XXXXXX)
 
 # Script Variables
 dtg=$(date '+%s')
-cacert="${__certs}/trust.pem"
+cacert="${tempdir}/trust.pem"
 
+# CA Details //User Defined
+cafqdn="twsldc205.gray.bah-csfc.lab"
+caprofile="eud"
+publicport="443"
+privateport="8443"
+estpuburl="https://${cafqdn}:${publicport}/certagent/est/${caprofile}/"
+estprivurl="https://${cafqdn}:${privateport}/certagent/est/${caprofile}/"
 
-# User Defined Variables
-cauri="https://twsldc205.gray.bah-csfc.lab/certagent/est/ca7/"
-cnvalue="DemoCN"
-
-# Load variables from external config
-#source ${__dir}/est.conf
+# Client Details 
+clientcert="/path/to/user_cert.p12"  # User Defined
+p12pw=${1:-} 
+cnvalue=$(openssl x509 -noout -subject -in /home/acavella/stackexchange.pem -nameopt multiline | grep commonName | awk '{ print $3 }')
 
 ######## FUNCTIONS #########
-# All operations are built into individual functions for better readibility
-# and management.  
 
-show_version() {
-    printf "EST-SimpleReenroll version ${VERSION}"
-    printf "Bash  version ${BASH_VERSION}"
-    printf "${DETECTED_OS}"
-    exit 0
+show_variables() {
+    echo ${estpuburl}
+    echo ${estprivurl}
+    echo ${cnvalue}
+    echo ${p12pw}
+    echo ${tempdir}
 }
 
-show_help() {
-    echo -e "
-    Usage: ./repr.sh [OPTION]
-    Syncs with remote RPM repo and creates incremental update packages for use with an offline repository.
-        -u, --update        execute standard update process
-            --help          display this help and exit
-            --version       output version information and exit
-    Examples:
-        ./repr -u  Downloads latest RPMs and creates a tarball.
-    "
-    exit 0
-}
-
-get_cacerts() {
+get_cacert() {
     curl --insecure ${cauri}/cacerts -v -o ${cacert}
 }
 
+submit_request() {
+    openssl pkcs12 extract public cert
+    openssl pkcs12 extract private key
+    
+}
 
+show_variables
